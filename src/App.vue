0858dcbf0e26b31b5647afce7a15f57255c0b0ad
Arturo Saldivar <damonbalam@gmail.com>
2020-02-10 17:54:15 -0500

    estructura del dashboard iniciada

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,39 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <!-- menu lateral -->
+    <SideComponent />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+    <!-- barra de navegacion -->
+    <v-app-bar app color="light-blue darken-4" dark clipped-left="true">
+      <div class="d-flex align-center">
+        <h1>Sistema de Control</h1>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn href="#" target="_blank" text>
+        <span class="mr-2">US001</span>
+        <v-icon>mdi-account-circle</v-icon>
       </v-btn>
     </v-app-bar>
 
+    <!-- vista -->
     <v-content>
-      <HelloWorld />
+      <DashComponent />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import DashComponent from "./components/DashComponent";
+import SideComponent from "./components/SideComponent";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
+    DashComponent,
+    SideComponent
   },
 
   data: () => ({
---
+++ src/components/SideComponent.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    color="light-blue darken-4"
+    :expand-on-hover="expandOnHover"
+    :mini-variant="miniVariant"
+    :right="false"
+    :src="bg"
+    app
+    dark
+    clipped
+  >
+    <v-list dense nav class="py-0">
+      <v-list-item two-line :class="miniVariant && 'px-0'">
+        <v-list-item-avatar>
+          <img src="https://randomuser.me/api/portraits/men/81.jpg" />
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>Application</v-list-item-title>
+          <v-list-item-subtitle>Subtext</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Photos", icon: "mdi-image" },
+        { title: "About", icon: "mdi-help-box" }
+      ],
+      color: "primary",
+      colors: ["primary", "blue", "success", "red", "teal"],
+      right: false,
+      miniVariant: false,
+      expandOnHover: false,
+      background: false
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
