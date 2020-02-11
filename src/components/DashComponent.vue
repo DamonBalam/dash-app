62086f6d8fe2058146cb093a5ee2ce6a23778e1f
Arturo Saldivar <damonbalam@gmail.com>
2020-02-10 19:01:29 -0500

    cards design done!

--- src/components/DashComponent.vue
+++ src/components/DashComponent.vue
@@ -1,141 +1,56 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
+    <v-row class="text-center justify-space-around">
+      <v-col cols="12" class="text-left">
+        <span class="display-1 font-weight-bold ml-8">
+          Dashboard
+          <small class="grey--text">v1.1</small>
+        </span>
+      </v-col>
+      <v-col cols="2">
+        <WidgetComponent :countTotal="2386" :text="'TOTAL'" :porcent="100" />
+      </v-col>
+      <v-col cols="2">
+        <WidgetComponent
+          :countTotal="883"
+          :text="'INCIDENTES'"
+          :porcent="37"
+          :isColor="'#FF6D00'"
         />
       </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
+      <v-col cols="2">
+        <WidgetComponent
+          :countTotal="310"
+          :text="'PROBLEMAS'"
+          :porcent="13"
+          :isColor="'#BF360C'"
+        />
       </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
+      <v-col cols="2">
+        <WidgetComponent
+          :countTotal="764"
+          :text="'TAREAS'"
+          :porcent="32"
+          :isColor="'#311B92'"
+        />
       </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
+      <v-col cols="2">
+        <WidgetComponent
+          :countTotal="429"
+          :text="'CONSULTAS'"
+          :porcent="18"
+          :isColor="'green'"
+        />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import WidgetComponent from "./WidgetComponent";
 export default {
-  name: "HelloWorld",
-
-  data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader"
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify"
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify"
-      }
-    ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com"
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com"
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify"
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs"
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify"
-      }
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer"
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/layout/pre-defined"
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
-      }
-    ]
-  })
+  components: {
+    WidgetComponent
+  }
 };
 </script>
---
+++ src/components/WidgetComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-card
+    class="mx-auto my-2 "
+    max-width="300"
+    max-height="180"
+    outlined
+    shaped
+    :style="`border-color:${isColor}`"
+  >
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="title mb-1"
+          >{{ countTotal }}
+          <v-icon small :color="isColor"> {{ icon }}</v-icon>
+        </v-list-item-title>
+        <v-list-item-subtitle>{{ text }} </v-list-item-subtitle>
+        <v-list-item-subtitle>DE TICKETS</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80">
+        <v-progress-circular
+          :rotate="-90"
+          :size="80"
+          :width="3"
+          :value="porcent"
+          :color="isColor"
+        >
+          {{ porcent }}%
+        </v-progress-circular>
+      </v-list-item-avatar>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    countTotal: {
+      type: Number,
+      default: 0
+    },
+    text: {
+      type: String,
+      default: "TOTAL"
+    },
+    porcent: {
+      type: Number,
+      default: 100
+    },
+    isColor: {
+      type: String,
+      default: "#1976D2"
+    },
+    icon: {
+      type: String,
+      default: "mdi-cards"
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
